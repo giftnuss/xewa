2dfbd228aa6ce64d371afaf1157103189f12edcc
Sebastian B. Knapp <news@young-workers.de>
2021-09-29 01:30:05 -0400

    make isArray available from lodash

--- src/typecheck.mjs
+++ src/typecheck.mjs
@@ -2,10 +2,10 @@
 /**
  * Common type checks, mostly imported from lodash.
  */
- 
+import isArray from "lodash/isArray.js";
 import isFunction from "lodash/isFunction.js";
 import isNull from "lodash/isNull.js";
 
-export { isFunction, isNull };
+export { isArray, isFunction, isNull };
 
 export default function () { /* TODO */ };
